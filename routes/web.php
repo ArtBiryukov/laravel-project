bc32d1fe6ab37427778081c644b6b5e9e6bf4c02
Artem Biryukov <carbon0093@gmail.com>
2022-04-25 12:31:30 +0300

    home work first

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', function () {
+    return 'Home list';
+});
+
+Route::get('/info', function () {
+    $inform = phpinfo();
+    return $inform;
+});
