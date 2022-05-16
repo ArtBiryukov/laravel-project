0cf5c4173bf46e402006628938dd38586dfca8a3
Artem Biryukov <carbon0093@gmail.com>
2022-05-16 19:24:34 +0300

    Заглушка

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,16 +14,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/home', function () {
-    return 'Home list';
-});
-
-Route::get('/info', function () {
+Route::get('/infophp', function () {
     $inform = phpinfo();
     return $inform;
 });
+
+//Приветственная страница
+// Route::get('/', function () {
+//     return '
+//     <div class="title">Приветственная страница новостного портала</div>';
+// });
+
+Route::get('/', [HomeController::class, 'show']);
+
+// //Добавление новости
+// Route::get('/addnews', function () {
+//     return 'Добавление новостей';
+// });
+
+// //Новость категория
+// Route::get('/catnews', function ($id) {
+//     return 'Категории'.$id;
+// });
+
+// //Новость категория
+// Route::get('/catnews/{id}', function ($id) {
+//     return 'Категории'.$id;
+// });
+
+// //Конкретная новость
+// Route::get('/news/{catid}/{id}', function ($catid, $id) {
+//     return 'новость'.$id;
+// });
+
+
+Route::get('/news', [NewsController::class, 'index']);
+Route::get('/news/{id}', [NewsController::class, 'show']);
+
+//admin routes
+// Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
+// 	Route::get('/', AdminController::class)
+// 		->name('index');
+// 	Route::resource('/categories', AdminCategoryController::class);
+// 	Route::resource('/news', AdminNewsController::class);
+// });
